8612ac13251f063dd33b4a651aeaca4f6d6cf194
prajakta <prajaktaghavare@hotmail.com>
2019-05-04 17:16:46 +0530

    New Constructor application

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class CalculateSquare
+    {
+        int number;
+        public CalculateSquare()
+        {
+            number = 10;
+           square(number);
+
+        }
+        public void square(int number)
+        {
+            number *= number;
+            Console.WriteLine("square is {0}", number);
+
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CalculateSquare cal = new CalculateSquare();
+            Console.ReadLine();
+        }
+    }
+}
